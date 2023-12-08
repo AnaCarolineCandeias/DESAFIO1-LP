5d75aa9f770c14fdab7af8aa3f0561a024c86b1c
AnaCarolineCandeias <anacarolinecandeias@gmail.com>
2023-12-08 13:13:24 -0300

    Desafio 1 - Lógica de Programação

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Ano novo está chegando!!");
+
+let cantora = "Mariah Carey";
+
+let idade = 54;
+
+let ano = 2023;
+
+console.log(ano - idade);
+
+const anoNascimento = ano - idade;
+
+console.log(anoNascimento);
+
+let  quartaFeira = true;
+
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
+
+let _idade = 18;
+
+if (_idade >= 18) {
+    console.log(
+    "Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.");
+    } else {
+console.log ("Sinto muito, mas assistirá da tv globo");
+};
+
+let aniversario = "Abril";
+
+if (aniversario == "Agosto" || aniversario == "Dezembro" || aniversario == "Junho"){
+    console.log(`Uma ou mais opções estão corretas. O mês escolhido foi o mês ${aniversario}.`)
+} else {
+    console.log(`Algo de errado não está certo, o mês digitado foi o mês ${aniversario}.`)
+};
